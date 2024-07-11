c1c6813bdd175f2fd176ef72141c44921896526b
facufr12 <facufr2@hotmail.com>
2024-07-11 16:14:46 -0300

    linea rosa

--- app/components/nuevanav.tsx
+++ app/components/nuevanav.tsx
@@ -21,57 +21,53 @@ const Navbar = () => {
 
   return (
     <nav className="w-full bg-white shadow-lg fixed top-0 z-50">
-      <div className="max-w-screen-lg mx-auto px-4 py-2 md:py-4 flex items-center justify-between">
-        {/* Logo */}
-        <div className="flex-shrink-0">
-          <Image src={Logo} alt="Logo" width={150} height={100} />
-        </div>
-
-        {/* Menú principal */}
-        <div className="flex-grow text-black text-center md:text-right uppercase relative">
-          <div className="flex justify-center items-center md:hidden absolute left-20 right-0 top-0 bottom-0">
-            {!isOpenMenu && (
-              <button
-                onClick={toggleMenu}
-                className="text-gray-900 focus:outline-none  text-2xl md:text-base"
-              >
-                <FaBars />
-              </button>
-            )}
-          </div>
-          <ul
-            className={`md:flex mt-2 mr-4 justify-center md:justify-end space-x-4 md:space-x-8 font-medium ${
-              isOpenMenu ? "block" : "hidden"
-            }`}
-          >
-            <NavItem
-              href="https://medicals.ar/sucursales/"
-              onClick={toggleMenu}
-            >
-              Sucursales
-            </NavItem>
-            <NavItem
-              href="https://medicals.ar/quienes-somos/"
-              onClick={toggleMenu}
-            >
-              Quienes Somos
-            </NavItem>
-            <NavItem href="https://medicals.ar/cartilla/" onClick={toggleMenu}>
-              Cartilla
-            </NavItem>
-          </ul>
-          <div className="absolute top-0 right-0 md:relative md:static mt-2 md:mt-0"></div>
-        </div>
-
-        <button
-          onClick={scrollToForm}
-          className="px-6 ml-1 py-4 md:py-3 hidden md:block rounded-full bg-[#E82E8A] text-white text-xs  font-bold tracking-widest uppercase transform hover:scale-105 hover:bg-gray-700 transition-colors duration-200"
-        >
-          Solicitá Asesor
-        </button>
+    {/* Línea rosa encima del nav */}
+  <div className="bg-[#E82E8A] h-3 w-full"></div> {/* Línea de color #E82E8A */}
+  
+    <div className="max-w-screen-lg mx-auto px-4 py-2 md:py-4 flex items-center justify-between relative">
+      {/* Logo */}
+      <div className="flex-shrink-0">
+        <Image src={Logo} alt="Logo" width={150} height={100} />
       </div>
-      <div></div>
-    </nav>
+  
+      {/* Menú principal */}
+      <div className="flex-grow text-black text-center md:text-right uppercase">
+        <div className="flex justify-center items-center md:hidden absolute left-20 right-0 top-0 bottom-0">
+          {!isOpenMenu && (
+            <button
+              onClick={toggleMenu}
+              className="text-gray-900 focus:outline-none text-2xl md:text-base"
+            >
+              <FaBars />
+            </button>
+          )}
+        </div>
+        <ul
+          className={`md:flex mt-2 mr-4 justify-center md:justify-end space-x-4 md:space-x-8 font-medium ${
+            isOpenMenu ? "block" : "hidden"
+          }`}
+        >
+          <NavItem href="https://medicals.ar/sucursales/" onClick={toggleMenu}>
+            Sucursales
+          </NavItem>
+          <NavItem href="https://medicals.ar/quienes-somos/" onClick={toggleMenu}>
+            Quienes Somos
+          </NavItem>
+          <NavItem href="https://medicals.ar/cartilla/" onClick={toggleMenu}>
+            Cartilla
+          </NavItem>
+        </ul>
+      </div>
+  
+      <button
+        onClick={scrollToForm}
+        className="px-6 ml-1 py-4 md:py-3 hidden md:block rounded-full bg-[#E82E8A] text-white text-xs font-bold tracking-widest uppercase transform hover:scale-105 hover:bg-gray-700 transition-colors duration-200"
+      >
+        Solicitá Asesor
+      </button>
+    </div>
+  </nav>
+  
   );
 };
 
