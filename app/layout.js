730c19b6a3a504908c70dc43b63869ac5d01f4a0
facufr12 <facufr2@hotmail.com>
2024-07-18 10:59:53 -0300

    output

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,26 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-
 export const metadata = {
   title: "Medicals",
   description: "Generado por Facu.f",
 
-  
-    icons: {
-      icon: ['/favicon.png'],
-     
-    },
-  
-  
+  icons: {
+    icon: ["/favicon.png"]
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
-<meta name="theme-color" content="#E82E8A" />
-    <body style={{ fontFamily: "'Acumin Variable Concept ExtraCo', Arial, sans-serif" }}>
-      {children}
-    </body>
-  </html>
+      <meta name="theme-color" content="#E82E8A" />
+      <body
+        style={{
+          fontFamily: "'Acumin Variable Concept ExtraCo', Arial, sans-serif"
+        }}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 // next.config.js
 const nextConfig = {
+  output: 'export',
     // Aquí puedes incluir otras configuraciones de Next.js si las necesitas
   };
   
-  module.exports = {
-    ...nextConfig,
-    output: 'standalone',
-  };
-  
+
   // No es necesario exportar default nextConfig; en CommonJS no se utiliza esta sintaxis
   
