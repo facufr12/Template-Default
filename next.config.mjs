9cee711fa0bd5dda93e66e6f4a8895a5ccf7c652
facufr12 <facufr2@hotmail.com>
2024-07-18 12:02:54 -0300

    carpeta out creada

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-    // Aquí puedes incluir otras configuraciones de Next.js si las necesitas
-  };
-  
-
-  // No es necesario exportar default nextConfig; en CommonJS no se utiliza esta sintaxis
-  
+  output:'export',
+};
+/* module.exports = nextConfig*/
+export default nextConfig;
